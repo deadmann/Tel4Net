a26f50b3c839043ded8fd415397c2f639a3ef958
mimosh.pisholack <mimosh.pisholack@gmail.com>
2020-10-26 13:51:30 +0330

    Add Test Project to the Solution

---
+++ Test/Tel4Net.TestCore/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tel4Net.TestCore
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
